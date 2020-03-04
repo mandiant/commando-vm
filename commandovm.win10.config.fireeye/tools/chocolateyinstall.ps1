6b0421cc0e66d4a9b546375cf5725e30e3b37d2e
Jake Barteaux <jacob.barteaux@mandiant.com>
2020-03-03 16:31:29 -0800

    Updated config

--- commandovm.win10.config.fireeye/tools/chocolateyinstall.ps1
+++ commandovm.win10.config.fireeye/tools/chocolateyinstall.ps1
@@ -1,46 +1,8 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Continue'
 
 $packageName = 'commandovm.win10.config.fireeye'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-function PinToTaskbar {
-  # https://stackoverflow.com/questions/31720595/pin-program-to-taskbar-using-ps-in-windows-10
-  param (
-    [parameter(Mandatory=$True, HelpMessage="Target item to pin")]
-    [ValidateNotNullOrEmpty()]
-    [string] $Target
-  )
-  if (-Not (Test-Path $Target)) {
-    Write-Warning "$Target does not exist"
-    throw [System.IO.FileNotFoundException] "$Target does not exist"
-  }
-
-  $KeyPath1  = "HKCU:\SOFTWARE\Classes"
-  $KeyPath2  = "*"
-  $KeyPath3  = "shell"
-  $KeyPath4  = "{:}"
-  $ValueName = "ExplorerCommandHandler"
-  $ValueData =
-    (Get-ItemProperty `
-        ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\" + `
-            "CommandStore\shell\Windows.taskbarpin")
-    ).ExplorerCommandHandler
-
-  $Key2 = (Get-Item $KeyPath1).OpenSubKey($KeyPath2, $true)
-  $Key3 = $Key2.CreateSubKey($KeyPath3, $true)
-  $Key4 = $Key3.CreateSubKey($KeyPath4, $true)
-  $Key4.SetValue($ValueName, $ValueData)
-
-  $Shell = New-Object -ComObject "Shell.Application"
-  $Folder = $Shell.Namespace((Get-Item $Target).DirectoryName)
-  $Item = $Folder.ParseName((Get-Item $Target).Name)
-  $Item.InvokeVerb("{:}")
-
-  $Key3.DeleteSubKey($KeyPath4)
-  if ($Key3.SubKeyCount -eq 0 -and $Key3.ValueCount -eq 0) {
-    $Key2.DeleteSubKey($KeyPath3)
-  }
-}
 
 ### Commando Windows 10 Attack VM ###
 Write-Host "[+] Beginning host configuration..." -ForegroundColor Green
@@ -63,11 +25,16 @@ Start-Sleep -Seconds 2
 
 #### Remove Desktop Shortcuts ####
 Write-Host "[+] Cleaning up the Desktop" -ForegroundColor Green
-$shortcut_path = "C:\Users\Public\Desktop\Boxstarter Shell.lnk"
-Remove-Item $shortcut_path -Force | Out-Null
-Start-Sleep -Seconds 2
+$shortcut_path = "$Env:Public\Desktop\Boxstarter Shell.lnk"
+if (Test-Path $shortcut_path) { Remove-Item $shortcut_path -Force -ErrorAction Ignore | Out-Null }
 $shortcut_path = "$Env:USERPROFILE\Desktop\Microsoft Edge.lnk"
-Remove-Item $shortcut_path -Force | Out-Null
+if (Test-Path $shortcut_path) { Remove-Item $shortcut_path -Force  -ErrorAction Ignore | Out-Null }
+$shortcut_path = "$Env:USERPROFILE\Desktop\Google Chrome.lnk"
+if (Test-Path $shortcut_path) { Remove-Item $shortcut_path -Force  -ErrorAction Ignore | Out-Null }
+$shortcut_path = "$Env:USERPROFILE\Desktop\VirusTotal Uploader 2.2.lnk"
+if (Test-Path $shortcut_path) { Remove-Item $shortcut_path -Force  -ErrorAction Ignore | Out-Null }
+$shortcut_path = "$Env:Public\Desktop\Simple DNSCrypt.lnk"
+if (Test-Path $shortcut_path) { Remove-Item $shortcut_path -Force -ErrorAction Ignore | Out-Null }
 
 
 #### Add timestamp to PowerShell prompt ####
@@ -87,28 +54,34 @@ Set-Content -Path $profile -Value $psprompt
 #   Offending string: "Y21kIC9jICdzZXR4IFBST01QVCBDT01NQU5ETyRTJGQkcyR0JF8kcCQrJGcn"
 #   Resolves to: "cmd /c 'setx PROMPT COMMANDO$S$d$s$t$_$p$+$g'"
 iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Y21kIC9jICdzZXR4IFBST01QVCBDT01NQU5ETyRTJGQkcyR0JF8kcCQrJGcn"))) | Out-Null
-Write-Host "`t[+] Timestamps added to cmd prompt and PowerShell" -ForegroundColor Green
+Write-Host "[+] Timestamps added to cmd prompt and PowerShell" -ForegroundColor Green
 
+#### Fix shift+space in powershell
+# https://superuser.com/questions/1365875/shiftspace-not-working-in-powershell
+Set-PSReadLineKeyHandler -Chord Shift+Spacebar -Function SelfInsert
+Write-Host "[+] Fixed shift+space keybinding in PowerShell" -ForegroundColor Green
 
 #### Pin Items to Taskbar ####
 Write-Host "[-] Pinning items to Taskbar" -ForegroundColor Green
 # Explorer
 $target_file = Join-Path ${Env:WinDir} "explorer.exe"
 try {
-  PinToTaskbar $target_file
+  Write-Host "`tPinning $target_file to taskbar" -ForegroundColor Green
+  syspin.exe "$target_file" 5387
 } catch {
-  Write-Host "Could not pin $target_file to the tasbar"
+  Write-Host "`tCould not pin $target_file to the taskbar" -ForegroundColor Red
 }
 # CMD prompt
 $target_file = Join-Path ${Env:WinDir} "system32\cmd.exe"
 $target_dir = ${Env:UserProfile}
 $target_args = '/K "cd ' + ${Env:UserProfile} + '"'
-$shortcut = Join-Path ${Env:UserProfile} "temp\CMD.lnk"
+$shortcut = Join-Path ${Env:UserProfile} "temp\cmd.lnk"
 Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $target_file -Arguments $target_args -WorkingDirectory $target_dir -PinToTaskbar -RunasAdmin
 try {
-  PinToTaskbar $shortcut
+  Write-Host "`tPinning $target_file to taskbar" -ForegroundColor Green
+  syspin.exe "$shortcut" 5387
 } catch {
-  Write-Host "Could not pin $target_file to the tasbar"
+  Write-Host "`tCould not pin $target_file to the taskbar" -ForegroundColor Red
 }
 # Powershell
 $target_file = Join-Path (Join-Path ${Env:WinDir} "system32\WindowsPowerShell\v1.0") "powershell.exe"
@@ -117,20 +90,21 @@ $target_args = '-NoExit -Command "cd ' + "${Env:UserProfile}" + '"'
 $shortcut = Join-Path ${Env:UserProfile} "temp\PowerShell.lnk"
 Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $target_file -Arguments $target_args -WorkingDirectory $target_dir -PinToTaskbar -RunasAdmin
 try {
-  PinToTaskbar $shortcut
+  Write-Host "`tPinning $target_file to taskbar" -ForegroundColor Green
+  syspin.exe "$shortcut" 5387
 } catch {
-  Write-Host "Could not pin $target_file to the tasbar"
+  Write-Host "`tCould not pin $target_file to the taskbar" -ForegroundColor Red
 }
 
 
 #### Rename the computer ####
 Write-Host "[+] Renaming host to 'commando'" -ForegroundColor Green
 (Get-WmiObject win32_computersystem).rename("commando") | Out-Null
-Write-Host "`t[-] Make sure to restart the machine for this change to take effect" -ForegroundColor Yellow
-Write-Host "[+] Changing Desktop Background" -ForegroundColor Green
+Write-Host "`t[-] Change will take effect after a restart" -ForegroundColor Yellow
 
 
 #### Update background ####
+Write-Host "[+] Changing Desktop Background" -ForegroundColor Green
 # Set desktop background to black
 Set-ItemProperty -Path 'HKCU:\Control Panel\Colors' -Name Background -Value "0 0 0" -Force | Out-Null
 # Set desktop wallpaper using WallpaperChanger utility
@@ -168,7 +142,11 @@ foreach ($item in "0", "1", "2") {
 Write-Host "[+] Copying README.txt to Desktop" -ForegroundColor Green
 $fileReadme = Join-Path $toolsDir 'readme.txt'
 $desktopReadme = Join-Path ${Env:USERPROFILE} "Desktop\README.txt"
-Copy-Item $fileReadme $desktopReadme
+try {
+  Copy-Item $fileReadme $desktopReadme -Force -ErrorAction SilentlyContinue
+} catch {
+  Write-Host "Could not copy Readme to Desktop. Please visit github to review."
+}
 
 # Fix PATH issues with Python installers #18
 $paths = @(
@@ -191,13 +169,21 @@ foreach ($p in $paths) {
 }
 
 if ($env_path -ne $old_path) {
-    setx /M PATH $env_path
-    refreshenv
+    try {
+      setx /M PATH $env_path
+      refreshenv
+    } catch {
+      Write-Host "Could not set PATH properly, please submit GitHub issue." -ForegroundColor Red
+    }
 }
 
 # Remove desktop.ini files
-Get-ChildItem -Path (Join-Path ${Env:UserProfile} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
-Get-ChildItem -Path (Join-Path ${Env:Public} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
+try {
+  Get-ChildItem -Path (Join-Path ${Env:UserProfile} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
+  Get-ChildItem -Path (Join-Path ${Env:Public} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
+} catch {
+  Write-Host "Could not remove desktop.ini files"
+}
 
 # Use AutoHotKey to modify various settings
 $scripts = @(
@@ -213,6 +199,7 @@ ForEach ($name in $scripts) {
 # Should be PS >5.1 now, enable transcription and script block logging
 # More info: https://www.fireeye.com/blog/threat-research/2016/02/greater_visibilityt.html
 if ($PSVersionTable -And $PSVersionTable.PSVersion.Major -ge 5) {
+  Write-Host "Enabling PowerShell Script Block Logging" -ForegroundColor Green
   $psLoggingPath = 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell'
   if (-Not (Test-Path $psLoggingPath)) {
     New-Item -Path $psLoggingPath -Force | Out-Null
